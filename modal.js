bb1943580041063d2735cdd247de0e064c8ed268
Itskode <97164927+farrukhan11@users.noreply.github.com>
2026-05-08 11:58:04 +0500

    chore: update code structure for better readability and maintainability

--- modal.js
+++ modal.js
@@ -6,12 +6,52 @@
 
   const overlay = modal.querySelector("[data-homepage-modal-overlay]");
   const closeButton = modal.querySelector(".homepage-modal__close");
+  const modalImage = modal.querySelector(".homepage-modal__image");
+  const images = ["assets/img/modal-pic.jpg", "assets/img/modal-pic2.jpg"];
+  const switchDelayMs = 2500;
+  const transitionMs = 500;
+  let currentIndex = 0;
+  let sliderIntervalId = null;
+
+  if (modalImage) {
+    modalImage.src = images[currentIndex];
+    images.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+  }
+
   const openModal = () => {
     modal.classList.add("is-visible");
   };
 
   const closeModal = () => {
     modal.classList.remove("is-visible");
+  };
+
+  const switchModalImage = () => {
+    if (!modalImage) {
+      return;
+    }
+    modalImage.classList.add("is-sliding-out");
+
+    window.setTimeout(() => {
+      currentIndex = (currentIndex + 1) % images.length;
+      modalImage.src = images[currentIndex];
+      modalImage.classList.remove("is-sliding-out");
+      modalImage.classList.add("is-sliding-in");
+
+      window.requestAnimationFrame(() => {
+        modalImage.classList.remove("is-sliding-in");
+      });
+    }, transitionMs);
+  };
+
+  const startAutoSlider = () => {
+    if (!modalImage || sliderIntervalId) {
+      return;
+    }
+    sliderIntervalId = window.setInterval(switchModalImage, switchDelayMs);
   };
 
   const attachEvents = () => {
@@ -33,6 +73,7 @@
   const init = () => {
     attachEvents();
     window.requestAnimationFrame(openModal);
+    startAutoSlider();
   };
 
   if (document.readyState === "loading") {
